1a7b2da6d2f05e64816be22df7abbdf008f64012
urkejov1996 <urkejov1996@gmail.com>
2024-08-12 18:33:17 +0200

    added new endpoint for retrieving all users

--- src/main/java/com/example/useraccountmanager/repository/AccountRepository.java
+++ src/main/java/com/example/useraccountmanager/repository/AccountRepository.java
@@ -11,9 +11,6 @@ import java.util.Optional;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, String> {
 
-    @Query("select a from Account a where a.user.id = ?1")
-    Optional<Account> findByUserId(String userId);
-
     @Query("select a from Account a where a.id = ?1 and a.user.id = ?2")
     Optional<Account> findByIdAndUserId(String accountId, String userId);
 
