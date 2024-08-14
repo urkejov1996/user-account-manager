70c57faebad2b219542a9af3c9a5c262ab992db2
urkejov1996 <urkejov1996@gmail.com>
2024-08-14 13:10:05 +0200

    started adding security

---
+++ src/main/java/com/example/useraccountmanager/security/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.example.useraccountmanager.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(Customizer.withDefaults());
+        http.cors(Customizer.withDefaults());
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/v3/api-docs", "/swagger-ui/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/v1/users/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/v1/users/resetPassword/{email}").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+}
