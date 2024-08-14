e279f4cd13201967dd8a8bdc1cad35e7a7c39207
urkejov1996 <urkejov1996@gmail.com>
2024-08-14 13:16:19 +0200

    started adding security

--- src/main/java/com/example/useraccountmanager/UserAccountManagerApplication.java
+++ src/main/java/com/example/useraccountmanager/UserAccountManagerApplication.java
@@ -1,13 +1,41 @@
 package com.example.useraccountmanager;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @SpringBootApplication
+@RequiredArgsConstructor
 public class UserAccountManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserAccountManagerApplication.class, args);
 	}
 
+
+
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer(Environment environment) {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedHeaders("Access-Control-Allow-Origin",
+								"*",
+								"Access-Control-Allow-Methods",
+								"POST, GET, OPTIONS, PUT, DELETE",
+								"Access-Control-Allow-Headers",
+								"Origin, X-Requested-With, Content-Type, Accept")
+						.allowedOrigins("*")
+						.allowedMethods("*");
+			}
+		};
+
+	}
 }
